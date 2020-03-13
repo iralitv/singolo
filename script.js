a466df34de4db5ff7ee62e0df59dd3fa8d55c394
Iralitv <ira.litvinchuk@mifort.org>
2020-03-13 15:36:17 +0300

    feat: add infinite slider

--- script.js
+++ script.js
@@ -6,5 +6,30 @@ MENU.addEventListener('click', (event) => {
   );
 
   event.target.classList.add('active');
-})
+});
 
+// carousel
+
+let slideIndex = 1;
+const NEXT_SLIDE_BTN = document.getElementById('next');
+const PREV_SLIDE_BTN = document.getElementById('prev');
+let slides = document.getElementsByClassName('slide');
+
+showSlides(slideIndex);
+
+NEXT_SLIDE_BTN.addEventListener('click', () => {plusSlides(1)})
+PREV_SLIDE_BTN.addEventListener('click', () => {plusSlides(-1)})
+
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+function showSlides(index) {
+  if (index > slides.length) slideIndex = 1;
+  if (index < 1) slideIndex = slides.length;
+
+  for(let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex-1].style.display = 'flex';
+}
